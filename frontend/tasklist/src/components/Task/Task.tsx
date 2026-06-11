cf158ba1bdcecaec24f297534a3760100e1f5d92
André Franciosi <andref2210@hotmail.com>
2026-06-11 15:22:16 -0300

    Frontend first version

---
+++ frontend/tasklist/src/components/Task/Task.tsx
@@ -0,0 +1,10 @@
+import './Task.css'
+
+export default function Task({ title, description }) {
+    return (
+        <div className="task">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
+}
